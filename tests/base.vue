d49a77da7eecd1b59da02fb0a852c6319145f91d
Matthew Wagerfield <matthew@wagerfield.com>
2017-12-13 20:12:41 +0000

    added base test component

---
+++ tests/base.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="one"/>
+</template>
+
+<script>
+import rules from './base-rules'
+
+export default {
+  props: {
+    margin: {
+      type: Number
+    },
+    color: {
+      type: String
+    }
+  },
+  computed: {
+    one() {
+      return this.$fela.renderRule(rules.one, this)
+    }
+  }
+}
+</script>
