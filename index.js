15fa0a9b30855b30664845430b92d166a2bc03a6
Matthew Wagerfield <matthew@wagerfield.com>
2018-03-12 14:55:36 +0000

    updated error message

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 throw new Error([
-  'This package has moved to vue-fela and is no longer maintained.',
-  'Please install vue-fela instead.'
-].join('\n'))
+  '[vue-fela-plugin]',
+  'This package has moved to [vue-fela] and is no longer maintained.',
+  'Please install [vue-fela] instead.'
+].join(' '))
