d6752f25c0b72fcb94a1752bc531106f95ddf578
Matthew Wagerfield <matthew@wagerfield.com>
2018-03-26 18:15:53 +0100

    updated plugin example

--- example/plugins/fela.js
+++ example/plugins/fela.js
@@ -2,11 +2,4 @@ import Vue from 'vue'
 import VueFela from '../../index'
 import renderer from '../fela'
 
-Vue.use(VueFela)
-
-export default ({ app }, inject) => {
-  inject('fela', renderer)
-  app.provide = Object.assign({
-    fela: renderer
-  }, app.provide)
-}
+Vue.use(VueFela, { renderer })
